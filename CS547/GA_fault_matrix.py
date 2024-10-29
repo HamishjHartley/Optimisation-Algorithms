592e1b2bd00cdfea8d4b86a7549e1d6c752fdc95
Hamish Hartley <113518445+HamishjHartley@users.noreply.github.com>
2024-10-29 20:45:53 +0000

    Got it kind of running now.

--- CS547/GA_fault_matrix.py
+++ CS547/GA_fault_matrix.py
@@ -2,7 +2,7 @@ import string
 import random
 from random import shuffle
 
-# Identify test cases which have the highest coverage, 
+# Identify test cases which have the highest coverage
 
 # Input text file
 with open('C:/Users/theha/Documents/GitHub/Optimisation-Algorithms/CS547/newsmallfaultmatrix.txt') as f: lines = f.readlines()
@@ -14,7 +14,7 @@ matrix = lines[0:5]
 random.seed(42) 
 pop_size=10
 crossover_rate = 0.75
-mutation_rate = 0.03
+mutation_rate = 0.05
 
 # Processes input into usable state (Comma separated list)
 def process_input(input:list):
@@ -72,12 +72,6 @@ def eval_population(population:list):
         #print("Individual: ",population[i][0])
     return population
 
-proc_matrix = process_input(matrix) # Process the input
-
-population = gen_population(proc_matrix) # Generate population from processed input
-
-scored = eval_population(population)
-
 def fittest_individual(population:list):
     max_value = max(population, key=lambda x: x[1])
     print(max_value)
@@ -123,19 +117,22 @@ def mutate(population):
         
         for i in range(len(individual_list)):
             if random.uniform(0, 1) < mutation_rate:
-                individual_list[i] = random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits+ string.whitespace + string.punctuation)
+                print(individual_list[i])
+                swap = random.randint(0,len(individual_list)-1)
+                individual_list[i], individual_list[swap] = individual_list[swap],individual_list[i]
         
-        individual[0] = ''.join(individual_list)
+        #individual[0] = ''.join(individual_list)
     return population
 
 # Main GA loop
 def genetic_algorithm():
-    pop = gen_population()
-    scored_pop = eval_population(pop)
+    proc_matrix = process_input(matrix) # Process the input
+    population = gen_population(proc_matrix) # Generate population from processed input
+    scored_pop = eval_population(population)
     i = 0
     print("Generation",i)
     print(fittest_individual(scored_pop))
-    while fittest_individual(scored_pop)[1] < 0:
+    while fittest_individual(scored_pop)[1] < 1:
         i += 1
         new_pop = []
         new_pop = select_and_generate_new_population(scored_pop)
@@ -144,3 +141,5 @@ def genetic_algorithm():
         scored_pop = eval_population(pop)
         print("Generation",i)
         print(fittest_individual(scored_pop))
+
+genetic_algorithm()
